89662862405198938e13a046c69cbf47d4b41374
alyayanbikova <ayanbikova@gmail.com>
2018-06-14 16:56:46 -0400

    add comments

--- src/test/java/com/dice/KeywordSearch.java
+++ src/test/java/com/dice/KeywordSearch.java
@@ -2,6 +2,7 @@ package com.dice;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,44 +17,56 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class KeywordSearch {
 
 	public static void main(String[] args) {
+		//print time, when test started
+		System.out.println("Test started - " + LocalDateTime.now());
+		
 		
 		 WebDriverManager.chromedriver().setup();
-	       //invoke selenium webdriver
 	      
+	      //creating array list to hold keywords
 		 List<String> keywords = new ArrayList<>();
 	      
 	      String line;   
-	   
+	   //read file, using bufeered reader, add keywords in the list
 	     try ( FileReader fr = new FileReader("keywords.txt");
 	      BufferedReader bf = new BufferedReader(fr); )  {
 	    	  while ( (line = bf.readLine() ) != null) {
 	    		    keywords.add(line);
 	    		     }
-		} catch (Exception e) {
+		} catch (IOException e) {
 			System.out.println("IO");
 		}
 	    		    
-	    		 // catch (IOException e) {
-	    	 // System.out.println("IOException was caught");
-	      System.out.println(keywords.size());
-	      
-	      for(int i=0; i<keywords.size(); i++) {
-	    	   WebDriver driver1 = new ChromeDriver();
-	    	  //set universal wait time in case web page is slow
-		        driver1.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		        
-	    	  driver1.get("https://www.dice.com");
-	    	  
-	    	  String keyword = keywords.get(i);
-	    	  
-	      driver1.findElement(By.id("search-field-keyword")).sendKeys(keyword);
 	     
-	      driver1.findElement(By.id("search-field-location")).sendKeys("Tampa");
-	      
+	      //System.out.println(keywords.size()); 20
+	    /** Search for keywords on dice.com,
+	     * using loop 
+	     * location doesn't change
+	     */
+	      for(int i=0; i<keywords.size(); i++) {
+	    	  //creating driver object
+	    	   WebDriver driver1 = new ChromeDriver();
+	    	   //maximizing window
+	    	   driver1.manage().window().maximize();
+	    	  //set universal wait time in case web page is slow
+		   driver1.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		      //go to www.dice.com  
+	    	  driver1.get("https://www.dice.com");
+	    	  //assign array list item to a variable
+	    	  String keyword = keywords.get(i);
+	    	  //send variable keyword to a field Keyword
+	      driver1.findElement(By.id("search-field-keyword")).sendKeys(keyword);
+	     // send location 
+	      driver1.findElement(By.id("search-field-location")).sendKeys("Orlando");
+	      //click find jobs button
 	      driver1.findElement(By.id("findTechJobs")).click();
+	      //assign String value of CountId to a var
 	       String countOfPositions = driver1.findElement(By.id("posiCountId")).getText();
+	       //assign value of var String to an int var count
 	       int count = Integer.parseInt(countOfPositions.replaceAll(",", ""));
+	       //print out search result
 	    		 System.out.println("For keyword :" + keyword + " found : " + count + " positions");
+	    		 //close the object driver
 	    		 driver1.close();
 	    		 
 	      }
@@ -61,7 +74,7 @@ public class KeywordSearch {
 	      
 	      
 	      
-	      
+	      //print time when test ended
 	        System.out.println("Test complited - " + LocalDateTime.now());
 
 	}
